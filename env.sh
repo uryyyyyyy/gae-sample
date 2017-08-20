efad92ca2f885fecdf979f5bb43cb09086675be6
uryyyyyyy <koki305@gmail.com>
2017-08-21 00:42:34 +0900

    init

--- env.sh
+++ env.sh
@@ -3,5 +3,6 @@
 export PROJECT_ID=haha
 echo "PROJECT_ID = $PROJECT_ID"
 export VERSION=`TZ=Asia/Tokyo date +%Y%m%d_%H%M%S`-`git rev-parse --short HEAD`
-export GOPATH=`which dev_appserver.py | sed -e "s/dev_appserver.py//g"`../platform/google_appengine/goroot-1.8:`pwd`
+export GOROOT=`which dev_appserver.py | sed -e "s/dev_appserver.py//g"`../platform/google_appengine/goroot-1.8
+export GOPATH=`pwd`
 export GAE_ROOTDIR=`pwd`
